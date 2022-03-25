3511081a3ff7f5be299ead401496e3b7c12aa98e
NightSky <evangelistaxiv14@gmail.com>
2022-03-26 06:15:44 +0800

    Final commit

---
+++ app/src/main/java/com/diva/restofinder/model/ModelHighlights.kt
@@ -0,0 +1,7 @@
+package com.diva.restofinder.model
+
+import java.io.Serializable
+
+class ModelHighlights : Serializable {
+    var highlights: String? = null
+}
---
+++ app/src/main/java/com/diva/restofinder/utils/OnItemClickCallback.kt
@@ -0,0 +1,7 @@
+package com.diva.restofinder.utils
+
+import com.diva.restofinder.model.ModelMain
+
+interface OnItemClickCallback {
+    fun onItemMainClicked(modelMainHorizontal: ModelMain?)
+}
